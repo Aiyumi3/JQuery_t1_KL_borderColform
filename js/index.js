95fa674a8d47671fd0bf30f52142ce0e5d815fa9
Aiyumi Sunshine <62023811+Aiyumi3@users.noreply.github.com>
2022-01-26 01:45:37 +0200

    changes

--- js/index.js
+++ js/index.js
@@ -3,15 +3,20 @@ $(document).ready(function(){
     $("#check").click(function(){
         const val = $("#billing_phone").val();
         if((val.match(/[+]380[0-9]{9}/ig) && val.length === 13) || val.length === 0){
-            $("#billing_phone").css("background-color", "white");
-            $("#billing_phone").css("border", "thin solid black");
+            $("#billing_phone").css({
+                "background-color": "white",
+                "border": "thin solid black"
+            });
 
             $("#billing_phone").val("");
             $("p").text("phone number").css("color", "black");
         }else{
-            $("#billing_phone").css("background-color", "#ffbfbf");
-            $("#billing_phone").css("border", "thick solid #cc0033");
+            $("#billing_phone").css({
+                "background-color": "#ffbfbf",
+                "border": "thick solid #cc0033"
+            });
             $("p").text("error").css("color", "#cc0033");
         }
     });
 });
+
