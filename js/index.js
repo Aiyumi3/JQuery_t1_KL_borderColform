c6a095bc932e1a391abed973683f4898c2054923
Aiyumi Sunshine <rsomeonei@gmail.com>
2022-01-26 01:00:30 +0200

    changes

---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function(){
+    $("#check").click(function(){
+        const val = $("#billing_phone").val();
+        if((val.match(/[+]380[0-9]{9}/ig) && val.length === 13) || val.length === 0){
+            $("#billing_phone").css("background-color", "white");
+            $("#billing_phone").css("border", "thin solid black");
+
+            $("#billing_phone").val("").css("color", "black");
+            $("p").text("phone number");
+        }else{
+            $("#billing_phone").css("background-color", "#ffbfbf");
+            $("#billing_phone").css("border", "thick solid #cc0033");
+            $("p").text("error").css("color", "#cc0033");
+        }
+    });
+});
