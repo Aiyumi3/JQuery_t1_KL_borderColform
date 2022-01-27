c1a0e87d3451eff55e22f1033ac84790d94c7dfd
Aiyumi Sunshine <62023811+Aiyumi3@users.noreply.github.com>
2022-01-27 08:40:52 +0200

    added changes

--- js/index.js
+++ js/index.js
@@ -1,22 +1,39 @@
 
+/**
+ * Задача:
+ * На сайте поле ввода номера телефона проверяется на правильность
+ * номера по Украине библиотекой libphonenumber-js
+ * Если ввести короткий номер (напр. +38099123),
+ * библиотека выбросит ошибку TOO_SHORT
+ * Если каким-то образом в поле только буквы (напр. +йцукенгшщзхъ),
+ * либа выбросит NOT_A_NUMBER
+ * Нам необходимо перехватить эти ошибки и просто делать красный border
+ */
+
+   
+
 $(document).ready(function(){
     $("#check").click(function(){
         const val = $("#billing_phone").val();
-        if((val.match(/[+]380[0-9]{9}/ig) && val.length === 13) || val.length === 0){
-            $("#billing_phone").css({
-                "background-color": "white",
-                "border": "thin solid black"
-            });
-
+        try {
+            libphonenumber.parsePhoneNumberWithError(val, "UA");
             $("#billing_phone").val("");
-            $("p").text("phone number").css("color", "black");
-        }else{
-            $("#billing_phone").css({
-                "background-color": "#ffbfbf",
-                "border": "thick solid #cc0033"
-            });
-            $("p").text("error").css("color", "#cc0033");
+        }catch (error){
+            if(error instanceof libphonenumber.ParseError){
+                $("#billing_phone").css({
+                    "background-color": "#ffbfbf",
+                    "border": "thick solid #cc0033"
+                });
+                $("p").text("error").css("color", "#cc0033");
+                setTimeout(function () {
+                    $("#billing_phone").css({
+                        "background-color": "white",
+                        "border": "thin solid black"
+                    });
+                    $("#billing_phone").val("");
+                    $("p").text("phone number").css("color", "black");
+                }, 1700);
+            }
         }
     });
 });
-
